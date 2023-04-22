59bcc82bea3712fefafc0b1cda983d1f0554261b
Ritesh Panwar <riteshpanwar110@gmail.com>
2023-04-23 03:38:38 +0530

    Added Bert Model for Course Similarity Score

--- course_recommender/bert_model.py
+++ course_recommender/bert_model.py
@@ -2,26 +2,57 @@ from numpy.linalg import norm
 from sentence_transformers import SentenceTransformer
 import pandas as pd
 import numpy as np
-df = pd.read_csv("course_descriptions.csv", encoding= 'ISO-8859-1', low_memory=False, error_bad_lines=False)
-courses = list(df["Course"].unique())
-model = SentenceTransformer('sentence-transformers/bert-base-nli-mean-tokens')
 
-def sim_score(course_list, input):
+
+def sim_score(rank_list, input, courses, df, model):
     text_list = []
-    for course in course_list:
-        if course not in course_list:
-            continue
-        text_list.append(df.iloc[course]["Description"])
-    embeddings = model.encode(text_list)
-    given_vector = np.mean(embeddings, axis=0)
-    input_embedding = model.encode(df.iloc[input]["Description"])
-    return np.dot(given_vector,input_embedding)/(norm(given_vector)*norm(input_embedding))
+    # score = []
+    code_not_detected = []
+    for code in rank_list:
+        if code not in courses:
+            for course in courses:
+                if code in course:
+                    text_list.append(df.loc[df['Course']==course]["Description"].to_string())
+                    # print(code)
+        else:
+            text_list.append(df.loc[df['Course']==code]["Description"].to_string())
+    
+    if len(text_list) == 0:
+        return 0
+    
+    input_desc = ""
+    if input not in courses:
+        for course in courses:
+                if input in course:
+                    input_desc = df.loc[df['Course']==course]["Description"].to_string()
+    else:
+        input_desc = df.loc[df['Course']==input]["Description"].to_string()
 
-def main():
-    courses_list = ['CSE101', 'CSE201', 'MTH203', 'CSE641', 'SSH325', 'CSE564']
-    input_course = 'CSE643'
-    score = sim_score(courses_list, input_course)
-    print("Course Score:", score)
+    if input_desc == "":
+        return 0
+    
+    given_text = model.encode(text_list)
+    given_vector = np.mean(given_text, axis=0)
+    # print(given_vector.shape)
+    # given_vector = np.mean(embeddings, axis=0)
+    input_embedding = model.encode(input_desc)
+    # print(input_embedding)
+    cosine_score = (np.dot(given_vector,input_embedding)/(norm(given_vector)*norm(input_embedding)))
+    score = ((cosine_score + 1)/2)*100
+    
+    return np.round(score, 2)
 
-if __name__ == '__main__':
-    main()
+def matching_score(input_course, courses_list):
+    df = pd.read_csv("course_descriptions.csv")
+    # print(df.shape)
+    df = df.dropna(subset=['Name', 'Course', 'Description'])
+    courses = list(df["Course"].unique())
+    for idx, course in enumerate(courses):
+        courses[idx] = courses[idx].replace(" ", "")
+    model = SentenceTransformer('sentence-transformers/bert-base-nli-mean-tokens')
+    # courses_list = ['CSE101', 'CSE201', 'MTH203', 'CSE641', 'SSH325', 'CSE564']
+    # input_course = 'CSE350'
+    # print(df)
+    score = sim_score(courses_list, input_course, courses, df, model)
+    print(score)
+    return score
